192635162cf390862cfe3511f234fafe85a08c27
Wekoslav Stefanovski <swekster@gmail.com>
2023-10-09 19:36:18 +0200

    Lecture 3 - group 1

--- Lecture-03/g1/exercise-02.js
+++ Lecture-03/g1/exercise-02.js
@@ -1,5 +1,19 @@
 function fizzBuzz() {
-    // TODO: Write your code here.
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log(`${i} - FizzBuzz`);
+            continue;
+        }
+        if (i % 3 === 0) {
+            console.log(`${i} - Fizz`);
+            continue;
+        }
+        if (i % 5 === 0) {
+            console.log(`${i} - Buzz`);
+            continue;
+        }
+        console.log(i);
+    }
 }
 
 fizzBuzz();
---
+++ Lecture-03/g1/exercise-05.js
@@ -0,0 +1,12 @@
+function describe(person) {
+    // TODO: Return a string that describes the person object
+    return "";
+}
+
+const myself = {
+    // TODO: Add properties
+};
+
+const description = describe(myself);
+
+console.log(description);
