96afb8ec80708ca194e5a4c2fc8cd630606b7701
Wekoslav Stefanovski <swekster@gmail.com>
2023-10-10 19:34:34 +0200

    Lecture 3 - group 2

--- Lecture-03/g2/exercise-02.js
+++ Lecture-03/g2/exercise-02.js
@@ -1,5 +1,19 @@
 function fizzBuzz() {
-  // your code here
+  for (let index = 1; index <= 100; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+      console.log("FizzBuzz");
+      continue;
+    }
+    if (index % 3 === 0) {
+      console.log("Fizz");
+      continue;
+    }
+    if (index % 5 === 0) {
+      console.log("Buzz");
+      continue;
+    }
+    console.log(index);
+  }
 }
 
 fizzBuzz();
---
+++ Lecture-03/g2/exercise-05.js
@@ -0,0 +1,10 @@
+const myself = {
+  // TODO: add properties
+}
+
+function describe() {
+  // TODO: return a string like "My name is ..., I am ... years old and I live in ..."
+}
+
+const description = describe(myself);
+console.log(description);
