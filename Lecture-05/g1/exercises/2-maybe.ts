3e5f72c9468f63d6f33457c3213d913b92f9bc8f
Wekoslav Stefanovski <swekster@gmail.com>
2023-10-30 19:10:45 +0100

    Lecture 5, Group 1, Exercises

---
+++ Lecture-05/g1/exercises/2-maybe.ts
@@ -0,0 +1,8 @@
+/*
+- Write a function `maybeDoStuff` that accepts a boolean argument `shouldDoStuff`. It should return a promise that invokes the `resolve` function after 2 seconds if `shouldDoStuff` is `true`, and invokes the `reject` function otherwise.
+- Test your function by calling it and chaining a `.then` method that logs a message to the console, and a `.catch` method that logs an error message to the console.
+*/
+
+function maybeDoStuff(shouldDoStuff: boolean) {
+    // TODO: implement
+}
