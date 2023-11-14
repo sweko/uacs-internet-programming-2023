341f4498154e9664f7e00b0b5a7eb798422da22a
Wekoslav Stefanovski <swekster@gmail.com>
2023-11-14 17:39:50 +0100

    Lecture 07, Group 2, init angular app

---
+++ Lecture-07/g2/group-two/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'group-two-angular';
+
+  subtitle = 'This is a subtitle';
+}
